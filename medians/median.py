1a5bdbefacb8e48c81b92fea0c736bf09810fafc
Sebastian Seitz <Sebastian.Seitz85@gmail.com>
2013-12-04 13:26:31 +0100

    Added draft for Presentation on 05/12/13 and the source-files for the median-creation.

--- medians/median.py
+++ medians/median.py
@@ -4,31 +4,33 @@ import csv
 def main(argv):
 
     filename = argv[1] 
-    out_cols = ['queryid', 'matchid', 'percentage', 'alignment-len', 'mistmatches', 'gap-openings', 'q.start', 'q.end', 's.start', 's.end', 'e-value', 'bit-score']
+    out_cols = ['queryid', 'matchid', 'percentage', 'e-value', 'bit-score']
     
-    result = dict()
-
+    #result = dict()
+    
     fileToRead = open(filename,'r')
     data = fileToRead.read()
-    res = []
+    res = list()
     for hit in data.splitlines():
         hit_data = dict()
         counter = 0
         for col in hit.split():
             hit_data[out_cols[counter]] = col
             if out_cols[counter]=='e-value':
-                if col in result:
-                    result[col] += 1
-                else:
-                    result[col] = 1
-
+                #if col in result:
+                #    result[col] += 1
+                #else:
+                #    result[col] = 1
+	           res.append(float(col))
             counter += 1
-        res.append(hit_data)
+        #res.append(hit_data)
 
-    w = csv.writer(open("output.csv", "w"))
-    for key, val in result.items():
-        w.writerow([key, val])
-        
+    #w = csv.writer(open("hh_percentage_new.csv", "w"))
+    w = open("hh_eValue_new.csv","w")
+    #for key, val in result.items():
+    #    w.writerow([key, val])
+    for item in res:
+        w.write("%s\n" % item)
     
 
 if __name__ == "__main__":
