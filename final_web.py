6c770a293aa12d347854147c0fbec7ab0d5a2bb4
Sebastian Seitz <Sebastian.Seitz85@gmail.com>
2014-01-13 10:32:08 +0100

    fixed syntax-error in final_web

--- final_web.py
+++ final_web.py
@@ -1,12 +1,9 @@
 import sys
 from pp2ex.Alignment import Blast
 from pp2ex.Alignment import Hhblits
-from pp2ex.ResultComparison import ComparisonResult
-from pp2ex.ResultComparison import ResultFilterer
 from pp2ex.Hpo import HpoTree
 from pp2ex.Hpo import HpoTreeCreator
 from pp2ex.Hpo import HpoTreeCombiner
-from pp2ex.Evaluator import Evaluator
 
 def getdefaulTree(treecreator):
     # Top 73 termids
@@ -21,14 +18,11 @@ def main(argv):
     sequence = sys.argv[1:]
 
     hitscount = 7
-    inputfilename = 'target/sp_species.9606.tfa'
-    outputfilename = 'target/results'
 
     # Initializations
     treecreator = HpoTreeCreator()
     blastalign = Blast('blastdb/db.fasta')
     combiner = HpoTreeCombiner()
-    evaluator = Evaluator()
 
     # Read sequences from file
 
@@ -36,24 +30,24 @@ def main(argv):
     counter = 0
     nomatchescounter = 0
     
-        hits = blastalign.run(sequence, hitscount)
+    hits = blastalign.run(sequence, hitscount)
     
-        if len(hits) == 0:
-            prediction = getdefaulTree(treecreator)
-        else:
-            for hit in hits:
-                hit['tree'] = treecreator.constructTreeForUniprotId(hit['matchid'])
-            prediction = combiner.combineBasedOnScore(hits)
+    if len(hits) == 0:
+       prediction = getdefaulTree(treecreator)
+    else:
+       for hit in hits:
+           hit['tree'] = treecreator.constructTreeForUniprotId(hit['matchid'])
+       prediction = combiner.combineBasedOnScore(hits)
         
-        threshold = 0.2
-        # Write leave terms of prediction to file (with threshold 0.3)
-        for term in prediction.terms.itervalues():
-            if len(term.children) > 0:
-                continue
-            if term.score < threshold:
-                continue
-            row = ('%s\t%s\t%.2f\n' % (seqid, term.id, term.score))
-            result+=row
+       threshold = 0.2
+       # Write leave terms of prediction to file (with threshold 0.3)
+       for term in prediction.terms.itervalues():
+           if len(term.children) > 0:
+               continue
+           if term.score < threshold:
+               continue
+           row = ('%s\t%s\t%.2f\n' % (seqid, term.id, term.score))
+           result+=row
             
     result+='END'
     print result
